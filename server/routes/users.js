2d9dbfcb07cca6f2e82268c1c9cae40681d7582d
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-05-21 19:17:39 +0200

    [feature #166129294] add api for login

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import signup from '../controllers/users';
+import signin from '../controllers/signin';
 
 const router = express.Router();
 
 // signup
 router.post('/signup', signup);
+
+// signin
+router.post('/signin', signin);
 
 export default router;
