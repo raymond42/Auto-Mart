982696c2ed53344692cd7340903b7c2708ddf13f
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-05-23 20:30:05 +0200

    [feature #166130096] add API for viewing a specific car

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ads from '../controllers/ads';
 import auth from '../middleware/auth';
 import markCarSold from '../controllers/markCar';
+import getCar from '../controllers/specific';
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ router.post('/', auth, ads);
 
 // mark car post as sold
 router.patch('/:id', auth, markCarSold);
+
+// get a specific car
+router.get('/:id', auth, getCar);
 
 export default router;
