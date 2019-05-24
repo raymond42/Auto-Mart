a19c23da79b503779901ff532013004aafe9c90a
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-05-24 17:01:14 +0200

    [feature #166130162] modify some changes

--- server/controllers/unsold.js
+++ server/controllers/unsold.js
@@ -2,17 +2,9 @@ import cars from '../models/cars';
 
 const getUnsoldCars = (req, res) => {
   const unsoldCars = cars.filter(car => car.status === 'available');
-  if (unsoldCars.length) {
-    res.status(200).json({
-      status: 200,
-      data: unsoldCars,
-    });
-    return;
-  }
-  res.status(404).json({
-    status: 404,
-    error: 'not found',
+  res.status(200).json({
+    status: 200,
+    data: unsoldCars,
   });
 };
-
 export default getUnsoldCars;
