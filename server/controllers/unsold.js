7f7b7da31e29094c1b315d5343944301b7dbb779
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-05-24 02:13:24 +0200

    [chore #166226161] add unit tests for viewing all unsold cars

--- server/controllers/unsold.js
+++ server/controllers/unsold.js
@@ -2,16 +2,16 @@ import cars from '../models/cars';
 
 const getUnsoldCars = (req, res) => {
   const unsoldCars = cars.filter(car => car.status === 'available');
-  if (!unsoldCars.length) {
-    res.status(404).json({
-      status: 404,
-      error: 'there is no unsold cars',
+  if (unsoldCars.length) {
+    res.status(200).json({
+      status: 200,
+      data: unsoldCars,
     });
     return;
   }
-  res.status(200).json({
-    status: 200,
-    data: unsoldCars,
+  res.status(404).json({
+    status: 404,
+    error: 'not found',
   });
 };
 
